277e408d08a3f062fcf7ff593b8d64ca1eb73d92
yesmaybetrue <80052298+yesmaybetrue@users.noreply.github.com>
2022-08-11 10:55:01 +0530

    First commit

---
+++ src/Test1.java
@@ -0,0 +1,11 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10,b=5;
+		int sum=a+b;
+		System.out.println("Sum= "+sum);
+	}
+
+}
